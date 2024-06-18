e3a99cbe7251de1f3e23175f73b36024b2862dca
hafuu <hafuu46@gmail.com>
2024-06-19 06:09:27 +0900

    globalに公開する

--- src/Fumon.Gas/Program.fs
+++ src/Fumon.Gas/Program.fs
@@ -1,5 +1,15 @@
 module Program
 
 open Test
+open Fable.Core
 
 let x() = String.concat "," (hello())
+
+type GlobalExports = {
+    mutable x: unit -> string
+}
+
+[<Global>]
+let ``global``: GlobalExports = jsNative
+
+``global``.x <- x
