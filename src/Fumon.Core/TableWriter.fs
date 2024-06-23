68322ab611e33ebe5d9bce51fd464dddff2391eb
hafuu <hafuu46@gmail.com>
2024-06-24 08:50:21 +0900

    Noと備考を出力。最後の行に罫線が出力されないのを修正

--- src/Fumon.Core/TableWriter.fs
+++ src/Fumon.Core/TableWriter.fs
@@ -4,6 +4,8 @@ open Fumon.Core.Types
 open Fumon.Core.Types.Spreadsheet
 
 let writeHeaders (parameters: ParameterDefinition[]) (row: int) (startColumn: int) (sheet: ISheet): unit =
+    sheet.GetCell(row, startColumn - 1).SetValue(Some "No.")
+
     parameters
     |> Array.iteri (fun index definition ->
         let column = startColumn + index
@@ -11,12 +13,17 @@ let writeHeaders (parameters: ParameterDefinition[]) (row: int) (startColumn: in
         cell.SetValue(Some (definition.Name))
     )
 
+    sheet.GetCell(row, startColumn + parameters.Length).SetValue(Some "備考")
+
 let writeTable (parameters: ParameterDefinition[]) (table: Combination seq) (beginRow: int) (beginColumn: int) (sheet: ISheet): int =
     let mutable numRows = 0
     table
     |> Seq.iteri (fun rowOffset combination ->
-        numRows <- rowOffset
+        let rowNo = rowOffset + 1
         let row = beginRow + rowOffset
+
+        sheet.GetCell(row, beginColumn - 1).SetValue(Some (string rowNo))
+
         parameters
         |> Array.iteri (fun columnOffset parameter ->
             let column = beginColumn + columnOffset
@@ -24,12 +31,14 @@ let writeTable (parameters: ParameterDefinition[]) (table: Combination seq) (beg
             let cell = sheet.GetCell(row, column)
             cell.SetValue(Some value)
         )
+
+        numRows <- rowNo
     )
     numRows
 
 let writeBorder (beginRow: int) (beginColumn: int) (numRows: int) (numColumns: int) (sheet: ISheet): unit =
-    sheet.GetRange(beginRow, beginColumn, numRows + 1, numColumns).SetBorder(true, true, true, true, true, true, BorderStyle.Solid)
-    sheet.GetRange(beginRow, beginColumn, 1, numColumns).SetBorder(bottom = true, style = BorderStyle.Double)
+    sheet.GetRange(beginRow, beginColumn - 1, numRows + 1, numColumns + 2).SetBorder(true, true, true, true, true, true, BorderStyle.Solid)
+    sheet.GetRange(beginRow, beginColumn - 1, 1, numColumns + 2).SetBorder(bottom = true, style = BorderStyle.Double)
 
 let write (parameters: ParameterDefinition[]) (table: Combination seq) (beginRow: int) (beginColumn: int) (sheet: ISheet): unit =
     writeHeaders parameters beginRow beginColumn sheet
