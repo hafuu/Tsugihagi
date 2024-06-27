91b7d2130c94a2bfa07d5b1b512775c256a809d0
hafuu <hafuu46@gmail.com>
2024-06-27 11:00:54 +0900

    バージョンを返す関数を定義

--- src/Fumon.Gas/Program.fs
+++ src/Fumon.Gas/Program.fs
@@ -3,6 +3,7 @@ module Fumon.Gas.Program
 open Fable.Core
 open Fumon.Core
 open TypeDefinitions.Gas
+open System
 
 let generateDecisionTable() =
     SpreadsheetApp
@@ -11,9 +12,13 @@ let generateDecisionTable() =
     
 type GlobalExports = {
     mutable generateDecisionTable: unit -> unit
+    mutable engine: unit -> string
+    mutable version: unit -> string
 }
 
 [<Global>]
 let ``global``: GlobalExports = jsNative
 
 ``global``.generateDecisionTable <- generateDecisionTable
+``global``.engine <- fun () -> AssemblyVersionInformation.AssemblyProduct
+``global``.version <- fun () -> AssemblyVersionInformation.AssemblyVersion
